c43bd98d623df1078b09a1b0a718732bcc9b0e04
EmmaYOLO <emmayolo163.com>
2022-03-21 14:32:46 +0800

    Enabling CSRF Protection

--- src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -34,13 +34,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeRequests()
                     .antMatchers("/").permitAll()
 //                    .antMatchers("/login").permitAll()
                     .anyRequest().hasRole("USER").and()
                 .formLogin()
                     .loginPage("/login")
+                    .defaultSuccessUrl("/dashboard")
                     .permitAll()
                     .and()
                 .logout()
